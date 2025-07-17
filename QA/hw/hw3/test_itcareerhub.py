b96a72b28435513a90f6b241476d46beaef54be7
anaseller <seller.anst@gmail.com>
2025-07-17 10:10:35 +0200

    new hw

--- QA/hw/hw3/test_itcareerhub.py
+++ QA/hw/hw3/test_itcareerhub.py
@@ -1,53 +1,64 @@
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.common.exceptions import NoSuchElementException
-# 
-# @pytest.fixture()
-# def driver():
-#     driver = webdriver.Chrome()
-#     driver.maximize_window()
-#     yield driver
-#     driver.quit()
-# 
-# def test_elements(driver):
-#     driver.get("https://itcareerhub.de/ru")
-# 
-#     wait = WebDriverWait(driver, 10)
-# 
-#     # Проверка логотипа
-#     logo = driver.find_element(By.XPATH, '//img[contains(@src, "Group_3793.svg")]')
-#     assert logo.is_displayed(), "Логотип не отображается"
-# 
-#     # Проверка ссылок
-#     menu_texts = [
-#         "Программы",
-#         "Способы оплаты",
-#         "Новости",
-#         "О нас",
-#         "Отзывы"
-#     ]
-# 
-#     for text in menu_texts:
-#         try:
-#             link = driver.find_element(By.XPATH, f'//a[text()="{text}"]')
-#             assert link.is_displayed(), f'Ссылка "{text}" не отображается'
-#         except NoSuchElementException:
-#             assert False, f'Ссылка "{text}" не найдена на странице'
-# 
-#     # Проверка кнопок переключения языка
-#     lang_ru = driver.find_element(By.XPATH, '//a[contains(text(), "ru")]')
-#     lang_de = driver.find_element(By.XPATH, '//a[contains(text(), "de")]')
-#     assert lang_ru.is_displayed(), 'Кнопка RU не отображается'
-#     assert lang_de.is_displayed(), 'Кнопка DE не отображается'
-# 
-#     # Кликнуть по иконке с телефонной трубкой
-#     phone_icon = driver.find_element(By.XPATH, '//img[contains(@src, "Group_3800.svg")]')
-#     phone_icon.click()
-# 
-#     # Проверка текста после клика
-#     text_locator = '//div[contains(text(), "Если вы не дозвонились, заполните форму на сайте. Мы свяжемся с вами")]'
-#     popup_text = wait.until(EC.visibility_of_element_located((By.XPATH, text_locator)))
-#     assert popup_text.is_displayed(), "Текст после клика по трубке не отображается"
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from time import sleep
+
+
+@pytest.fixture
+def driver():
+    service = Service("C://Program Files/ChromDriver/chromedriver.exe")
+    options = Options()
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.set_window_size(1024, 768)
+    driver.get("https://itcareerhub.de/ru")
+    yield driver
+    driver.quit()
+
+
+def test_logo_exists(driver):
+    element = driver.find_element(
+        By.CSS_SELECTOR,
+        "img.tn-atom__img"
+    )
+    assert element.is_displayed()
+
+
+def test_links_exist(driver):
+    links = [
+        "Программы",
+        "Способы оплаты",
+        "Новости",
+        "О нас",
+        "Отзывы"
+    ]
+
+    for link in links:
+        element = driver.find_element(By.LINK_TEXT, link)
+        assert element.is_displayed()
+
+
+def test_lang_switch(driver):
+    lang_link = driver.find_element(By.LINK_TEXT, "de")
+    sleep(2)
+    lang_link.click()
+    sleep(2)
+    current_url = driver.current_url
+    sleep(2)
+    assert current_url == "https://itcareerhub.de/"
+
+
+def test_phone_icon(driver):
+    element = driver.find_element(
+        By.CSS_SELECTOR,
+        "img[imgfield='tn_img_1710153310161']"
+    )
+    element.click()
+    sleep(2)
+    res = driver.find_element(
+        By.CSS_SELECTOR,
+        'div[class="t396__elem tn-elem tn-elem__7679561671711363912027"]'
+    )
+    assert res.text == "Если вы не дозвонились, заполните форму на сайте. Мы свяжемся с вами"
+    assert res.is_displayed()
